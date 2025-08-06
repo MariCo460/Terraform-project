c36823257181495c3f4a0bbd7030042c24ce340a
MariCo460 <marisolcolombo2002@gmail.com>
2025-08-06 17:22:22 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,8 @@
-# Output the AWS region being used
 output "aws_region" {
   description = "The AWS region where resources will be created"
-  value       = us-east-2
+  value       = var.aws_region  # Correctly referencing the variable
 }
 
-# Static output message
 output "welcome_message" {
   description = "A welcome message for the user"
   value       = "Welcome to your Terraform project!"
